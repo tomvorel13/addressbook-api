9090faaefdb467a1a0aed6db3542b43d76dbb0fc
Tom Vorel <tomvorel13@gmail.com>
2021-04-03 14:55:34 +0200

    Basic server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hi there!')
+import express from 'express'
+
+const app = express()
+const PORT = 8000
+app.get('/', (req, res) => res.send('Express + TypeScript Server'))
+app.listen(PORT, () => {
+    console.log(`Server is running at https://localhost:${PORT}`)
+})
